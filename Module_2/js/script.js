a9f755eb3c8b661f62febf356e62c530d47a025d
elatero <dev.elatero@gmail.com>
2020-07-09 19:22:14 +0300

    practise module_2 final

--- Module_2/js/script.js
+++ Module_2/js/script.js
@@ -90,4 +90,133 @@ window.addEventListener('DOMContentLoaded', function () {
   }
 
   setClock('.timer', deadline)
+
+  // Modal window
+
+  const modalTrigger = document.querySelectorAll('[data-modal]'),
+    modal = document.querySelector('.modal'),
+    modalCloseBtn = document.querySelector('[data-close]')
+
+  function toggleModalWindow() {
+    modal.classList.toggle('show')
+
+    if (modal.matches('.show')) {
+      document.body.style.overflow = 'hidden'
+      clearInterval(modalTimerId)
+    } else {
+      document.body.style.overflow = ''
+    }
+  }
+
+  modalTrigger.forEach((btn) =>
+    btn.addEventListener('click', toggleModalWindow)
+  )
+
+  modalCloseBtn.addEventListener('click', toggleModalWindow)
+
+  modal.addEventListener('click', (e) => {
+    const target = e.target
+    if (target && target === modal) {
+      toggleModalWindow()
+    }
+  })
+
+  document.addEventListener('keydown', (e) => {
+    if (e.code === 'Escape' && modal.classList.contains('show')) {
+      toggleModalWindow()
+    }
+  })
+
+  const modalTimerId = setTimeout(toggleModalWindow, 3000)
+
+  function showModalByScroll() {
+    if (
+      window.pageYOffset + document.documentElement.clientHeight >=
+      document.documentElement.scrollHeight
+    ) {
+      toggleModalWindow()
+      window.removeEventListener('scroll', showModalByScroll)
+    }
+  }
+
+  window.addEventListener('scroll', showModalByScroll)
+
+  // Use class
+
+  class MenuCard {
+    constructor(
+      src,
+      alt,
+      title,
+      description,
+      price,
+      parentSelector,
+      ...classes
+    ) {
+      this.src = src
+      this.alt = alt
+      this.title = title
+      this.description = description
+      this.price = price
+      this.classes = classes
+      this.parent = document.querySelector(parentSelector)
+      this.transfer = 27
+      this.changeToUAH()
+    }
+
+    changeToUAH() {
+      this.price = this.price * this.transfer
+    }
+
+    render() {
+      const element = document.createElement('div')
+
+      if (this.classes.length === 0) {
+        element.classList.add('menu__item')
+      } else {
+        this.classes.forEach((className) => element.classList.add(className))
+      }
+
+      element.innerHTML = `      
+        <img src=${this.src} alt=${this.alt}/>
+        <h3 class="menu__item-subtitle">${this.title}</h3>
+        <div class="menu__item-descr">${this.description}</div>
+        <div class="menu__item-devider"></div>
+        <div class="menu__item-price">
+          <div class="menu__item-cost">Цена:</div>
+          <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+        </div>    
+      `
+      this.parent.append(element)
+    }
+  }
+
+  new MenuCard(
+    'img/tabs/vegy.jpg',
+    'vegy',
+    'Меню "Фитнес"',
+    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+    9,
+    '.menu .container'
+  ).render()
+
+  new MenuCard(
+    'img/tabs/post.jpg',
+    'post',
+    'Меню "Постное"',
+    'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+    14,
+    '.menu .container',
+    'menu__item'
+  ).render()
+
+  new MenuCard(
+    'img/tabs/elite.jpg',
+    'elite',
+    'Меню “Премиум”',
+    'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+    21,
+    '.menu .container',
+    'menu__item'
+  ).render()
 })
