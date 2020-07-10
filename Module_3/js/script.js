0933c5c31b19ce46392cc8c207b90218a9cba8bf
elatero <dev.elatero@gmail.com>
2020-07-10 21:07:02 +0300

    practise module_3

--- Module_3/js/script.js
+++ Module_3/js/script.js
@@ -127,7 +127,7 @@ window.addEventListener('DOMContentLoaded', function () {
     }
   })
 
-  const modalTimerId = setTimeout(toggleModalWindow, 3000)
+  const modalTimerId = setTimeout(toggleModalWindow, 300000)
 
   function showModalByScroll() {
     if (
@@ -219,4 +219,83 @@ window.addEventListener('DOMContentLoaded', function () {
     '.menu .container',
     'menu__item'
   ).render()
+
+  // Forms
+
+  const forms = document.querySelectorAll('form')
+  const message = {
+    loading: 'img/form/spinner.svg',
+    success: 'Спасибо! Скоро мы с вами свяжемся',
+    failure: 'Что-то пошло не так...'
+  }
+
+  forms.forEach((item) => {
+    postData(item)
+  })
+
+  function postData(form) {
+    form.addEventListener('submit', (e) => {
+      e.preventDefault()
+
+      let statusMessage = document.createElement('img')
+      statusMessage.src = message.loading
+      statusMessage.style.cssText = `
+        display: block;
+        margin: 0 auto;
+      `
+      form.insertAdjacentElement('afterend', statusMessage)
+
+      const request = new XMLHttpRequest()
+      request.open('POST', 'server.php')
+      request.setRequestHeader(
+        'Content-type',
+        'application/json; charset=utf-8'
+      )
+      const formData = new FormData(form)
+
+      const object = {}
+      formData.forEach((value, key) => {
+        object[key] = value
+      })
+
+      const json = JSON.stringify(object)
+
+      request.send(json)
+
+      request.addEventListener('load', () => {
+        if (request.status === 200) {
+          console.log(request.response)
+          showThanksModal(message.success)
+          statusMessage.remove()
+          form.reset()
+        } else {
+          showThanksModal(message.failure)
+        }
+      })
+    })
+  }
+
+  function showThanksModal(message) {
+    const prevModalDialog = document.querySelector('.modal__dialog')
+
+    prevModalDialog.classList.add('hide')
+    toggleModalWindow()
+
+    const thanksModal = document.createElement('div')
+    thanksModal.classList.add('modal__dialog')
+    thanksModal.innerHTML = `
+      <div class="modal__content">
+        <div class="modal__close" data-close></div>
+        <div class="modal__title">${message}</div>
+      </div>
+    `
+
+    document.querySelector('.modal').append(thanksModal)
+    setTimeout(() => {
+      thanksModal.remove()
+      prevModalDialog.classList.add('show')
+      prevModalDialog.classList.remove('hide')
+      toggleModalWindow()
+    }, 4000)
+  }
 })
